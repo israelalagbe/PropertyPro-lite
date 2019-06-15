0c2fd4efeecf0d519b4959aa4a6791ff8ec19dc6
israelalagbe <israelalagbe53@gmail.com>
2019-06-15 16:47:26 +0100

    Navbar harmbugger transform

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,6 +1,7 @@
-var navControl = document.querySelector('nav .nav-control');
+const navControl = document.querySelector('nav .nav-control');
 navControl.addEventListener('click', () => {
     document.querySelectorAll('.nav-collapse').forEach((item) => {
         item.classList.toggle("show-nav-item")
     })
+    navControl.classList.toggle('change')
 });
